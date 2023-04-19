b610c46973cfb660f4f8d00d9514676cbe25bdb7
Zhenya Karelin <karel.zhenya1257@mail.ru>
2023-04-19 22:22:17 +0300

    Проект Место

---
+++ script/index.js
@@ -0,0 +1,30 @@
+let popupElement = document.querySelector('.popup');
+let popupClose = popupElement.querySelector('.popup__close');
+let popupOpen = document.querySelector('.profile__button');
+let infoSave = popupElement.querySelector('.popup__form');
+let inputName = popupElement.querySelector('.popup__name');
+let inputJob = popupElement.querySelector('.popup__info');
+let infoName = document.querySelector('.profile__name');
+let infoJob = document.querySelector('.profile__job');
+
+
+let openPopup = function () {
+    popupElement.classList.add('popup_opened');
+    inputName.value = infoName.textContent;
+    inputJob.value = infoJob.textContent;
+}
+
+let closePopup = function () {
+    popupElement.classList.remove('popup_opened');
+}
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    infoName.textContent = inputName.value;
+    infoJob.textContent = inputJob.value;
+    closePopup();
+}
+
+popupOpen.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
+infoSave.addEventListener('submit', handleFormSubmit);
