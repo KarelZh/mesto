37b86140cceac18fd06a0b15fb740d21a7a5efd5
Zhenya <karel.zhenya1257@mail.ru>
2023-08-11 18:35:15 +0300

    ca

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,32 +6,8 @@ import { Popup } from '../components/Popup.js';
 import { PopupWithImage } from '../components/PopupWithImage.js';
 import { PopupWithForm } from '../components/PopupWithForm.js';
 import { UserInfo } from '../components/UserInfo.js';
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
+import { Api } from '../components/Api.js';
+
 const configForm = {
   formSelector: '.popup__form',
   inputSelector: '.popup__info',
@@ -54,11 +30,32 @@ const popupImage = document.querySelector('.popup_type_image');
 const imageOpen = popupImage.querySelector('.popup__image');
 const imageText = popupImage.querySelector('.popup__name');
 
+//Переменные попапа удаления карточки
+const buttonDeleteCard = document.querySelector('.popup__button_type_delete');
+
+//Переменные попапа смены аватара
+const popupAvatar = document.querySelector('.popup_type_avatar')
+const popupAddAvatar = document.querySelector('.profile__content')
+const formAvatar = popupAvatar.querySelector('.popup__form_type_avatar')
+
+
+const api = new Api({
+  baseUrl: 'https://nomoreparties.co/v1/cohort-72',
+  headers: {
+    authorization: '9430b557-5691-4068-a9d3-25e6e6adc7cd',
+    'Content-Type': 'application/json'
+  }
+});
 //форма отправки добавления карточки
 const popupFormCard = new PopupWithForm({
   selector: '.popup_type_card',
   submit: (item) => {
-    cardsList.addItem(createCard(item));;
+    api.generateCard(item.name, item.link, item.likes).then((res) => {
+      cardsList.newAddItem(createCard(res))
+    })
+    .catch((err) => {
+      console.error(err)
+    })
     formCardValidator.disabledButton();
     popupFormCard.close();
   }
@@ -67,6 +64,7 @@ const popupFormCard = new PopupWithForm({
 popupAdd.addEventListener('click', () => {
   popupFormCard.open();
 });
+
 
 
 popupFormCard.setEventListeners(); 
@@ -78,14 +76,18 @@ const popupImageCard = new PopupWithImage({
 });
 const userInfo = new UserInfo({
   userNameSelector: '.profile__name',
-  userJobSelector: '.profile__job'
+  userJobSelector: '.profile__job',
+  userUrlSelector: '.profile__image'
 });
 const popupEditProfile = new PopupWithForm({
   selector: '.popup_type_information', 
   submit: (item) => {
     userInfo.setUserInfo(item);
+    api.newInfoUser(item.name, item.about).catch((err) => {
+      console.error(err)
+    })
     popupEditProfile.close();
-  },
+  }
 });
 //открытие попапа информации о себе
 popupOpen.addEventListener("click", () => {
@@ -98,6 +100,29 @@ popupOpen.addEventListener("click", () => {
 
 popupEditProfile.setEventListeners();
 
+const newPopup = new Popup({
+  selector: '.popup_type_delete'
+})
+//Функция открытия попапа удаления карточки
+function deleteCardClick() {
+  newPopup.open()
+}
+//Функция закрытия попапа удаления карточки
+function closeCardClick() {
+  newPopup.close()
+}
+// Функция удаления карточки
+function deleteCardId(item) {
+  api.deleteCard(item).catch((err) => {
+    console.error(err)
+  })
+}
+
+
+
+
+
+newPopup.setEventListeners();
 
 //открытие попапа с картинкой
 function handleCardClick(data) { 
@@ -106,29 +131,89 @@ function handleCardClick(data) {
 
 popupImageCard.setEventListeners();
 
-
+const popupFormAvatar = new PopupWithForm({
+  selector: '.popup_type_avatar',
+  submit: (item) => {
+    api.addAvatar(item.avatar).then((res) => {
+      userInfo.userPhoto(res.avatar)
+    }).catch((err) => {
+      console.error(err)
+    })
+    formAvatarValidator.disabledButton();
+    popupFormAvatar.close();
+  }
+})
+//Открытие попапа аватара
+popupAddAvatar.addEventListener('click', () => {
+  popupFormAvatar.open();
+})
+popupFormAvatar.setEventListeners();
 
 
 const cardsContainer = document.querySelector('.elements');
 
 function createCard(item) {
-  const newCard = new Card(item, '.element__template', handleCardClick); 
+  const newCard = new Card(
+    item,
+    '.element__template', 
+    handleCardClick, 
+    deleteCardClick, 
+    buttonDeleteCard, 
+    closeCardClick,
+    userID,
+    deleteCardId,
+    function likeCardApi(item) {
+      api.likeCard(item).then((res) => {
+        newCard.updateLikes(res.likes)
+      }).catch((err) => {
+        console.error(err)
+      })
+    },
+    function deleteLikeCardApi(item) {
+      api.deleteLikeCard(item).then((res) => {
+        newCard.updateLikes(res.likes)
+      }).catch((err) => {
+        console.error(err)
+      })
+    }); 
   return newCard.generateCard();
 }
 
+
+
 const formProfileValidator = new FormValidator(configForm, formInformation);
 const formCardValidator = new FormValidator(configForm, formCard);
+const formAvatarValidator = new FormValidator(configForm, formAvatar);
 formProfileValidator.enableValidation();
 formCardValidator.enableValidation();
+formAvatarValidator.enableValidation();
 
+
+
+let userID;
 
 const cardsList = new Section({
-  items: initialCards,
   renderer: (item) => {
     cardsList.addItem(createCard(item));
   },
 }, '.elements');
-cardsList.renderItems();
+api.infoUser().then((res) => {
+  userInfo.setUserInfo(res)
+  userInfo.userPhoto(res)
+})
+api.infoUser()
+.then((res) => {
+  userID = res._id;
+  console.log(userID)
+})
+.catch((err) => {
+  console.error(err);
+})
 
-
-console.log('Hello, World!')
+api.initialCards()
+.then((res) => {
+  console.log(res)
+  cardsList.renderItems(res)
+}).catch((err) => {
+  console.error(err)
+})
